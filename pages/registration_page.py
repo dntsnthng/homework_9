e4aa985e2d40d06b10ea813acf2a95a647feb06d
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-04-04 10:06:39 +0300

    refactor image path, delete label

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,5 +1,6 @@
-import os
 from selene import browser, command, have
+import resource
+
 
 class RegistrationPage:
     def open(self):
@@ -10,15 +11,15 @@ class RegistrationPage:
         browser.element('#firstName').type(user.first_name)
         browser.element('#lastName').type(user.last_name)
         browser.element('#userEmail').type(user.email)
-        browser.element('label[for="gender-radio-2"]').click()
+        browser.element('[for="gender-radio-2"]').click()
         browser.element('#userNumber').type(user.number)
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').type(user.month)
         browser.element('.react-datepicker__year-select').type(user.year)
         browser.element(f'.react-datepicker__day--00{user.day}').click()
         browser.element('#subjectsInput').type(user.subjects).press_enter()
-        browser.element('label[for="hobbies-checkbox-3"]').click()
-        browser.element('#uploadPicture').send_keys(os.path.abspath('images/Screenshot_1.png'))
+        browser.element('[for="hobbies-checkbox-3"]').click()
+        browser.element('#uploadPicture').send_keys(resource.path('Screenshot_1.png'))
         browser.element('#currentAddress').type(user.address)
         browser.element('#react-select-3-input').type(user.state).press_enter()
         browser.element('#react-select-4-input').type(user.city).press_enter()
